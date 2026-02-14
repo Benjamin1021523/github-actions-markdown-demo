8d4b2b4476ff52bdbb591ed32e720ceb92bb6eb8
benjamin <benjaminchang@crm.com.tw>
2026-02-14 10:27:37 +0800

    修正設定的路徑

--- md-docs/.vitepress/config.mjs
+++ md-docs/.vitepress/config.mjs
@@ -1,7 +1,7 @@
 export default {
   // 這裡的 'repo-name' 請改為你在 GitHub 上的 Repository 名稱
   // 記得前後都要有斜線 /
-  base: '/嘗試vitepress/', 
+  base: '/github-actions-markdown-demo/', 
   
   title: "我的技術文件",
   description: "使用 VitePress 製作"
